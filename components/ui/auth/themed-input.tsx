61078afc0539cbfcb187ade3475729f504a59add
Abhishek <abhishekjha96500@gmail.com>
2025-10-10 23:19:40 +0530

    completed the themed input custom component.

--- components/ui/auth/themed-input.tsx
+++ components/ui/auth/themed-input.tsx
@@ -1,4 +1,5 @@
 import { ThemedView } from "@/components/themed-view";
+import { useState } from "react";
 import { Image, ImageSourcePropType, StyleSheet, TextInput, useWindowDimensions } from "react-native";
 
 interface ThemedInputProps {
@@ -6,23 +7,51 @@ interface ThemedInputProps {
     placeHolder: string
 }
 
-export default function ThemedInput({imgPath, placeHolder}: ThemedInputProps) {
+export default function ThemedInput({ imgPath, placeHolder }: ThemedInputProps) {
     const { width } = useWindowDimensions();
+    const [isFocused, setIsFocused] = useState(false);
     const inputBoxWidth = width < 360 ? width * 0.8 : 320;
 
-  return (
-    <ThemedView style={[styles.container, { width: inputBoxWidth }]}>
-        <Image source={imgPath} />
-        <TextInput placeholder={placeHolder}></TextInput>
-    </ThemedView>
-  )
+
+    return (
+        <ThemedView
+            style={[
+                styles.container,
+                { width: inputBoxWidth },
+                isFocused && styles.focusedContainer, // highlight border when focused
+            ]}
+        >
+            <Image source={imgPath} />
+            <TextInput
+                style={styles.inputContainer}
+                placeholder={placeHolder}
+                placeholderTextColor="rgba(255, 255, 255, 0.5)" // less opaque placeholder
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                underlineColorAndroid="transparent"
+            />
+        </ThemedView>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-        display: 'flex',
         flexDirection: 'row',
-        maxWidth: 324,
-        maxHeight: 72,
-    }
+        height: 56,
+        backgroundColor: '#343435',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        borderRadius: 12,
+        gap: 20,
+        borderWidth: 1,
+        borderColor: 'transparent',
+    },
+    focusedContainer: {
+        borderColor: '#B3B1FF', // highlight color when input is focused
+    },
+    inputContainer: {
+        flex: 1,
+        height: 24,
+        color: 'white',
+    },
 })
