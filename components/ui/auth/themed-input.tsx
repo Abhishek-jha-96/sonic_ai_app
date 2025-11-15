e101bb95a302bbf5c7034eb7beb76fd31135b677
Abhishek <abhishekjha96500@gmail.com>
2025-11-16 00:06:06 +0530

    user create api integration.

--- components/ui/auth/themed-input.tsx
+++ components/ui/auth/themed-input.tsx
@@ -1,57 +1,74 @@
 import { ThemedView } from "@/components/themed-view";
 import { useState } from "react";
-import { Image, ImageSourcePropType, StyleSheet, TextInput, useWindowDimensions } from "react-native";
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+} from "react-native";
 
 interface ThemedInputProps {
-    imgPath: ImageSourcePropType
-    placeHolder: string
+  imgPath: ImageSourcePropType;
+  placeHolder: string;
+  value: string;
+  onChangeText: (text: string) => void;
+  secureTextEntry?: boolean;
 }
 
-export default function ThemedInput({ imgPath, placeHolder }: ThemedInputProps) {
-    const { width } = useWindowDimensions();
-    const [isFocused, setIsFocused] = useState(false);
-    const inputBoxWidth = width < 360 ? width * 0.8 : 320;
+export default function ThemedInput({
+  imgPath,
+  placeHolder,
+  value,
+  onChangeText,
+  secureTextEntry,
+}: ThemedInputProps) {
+  const { width } = useWindowDimensions();
+  const [isFocused, setIsFocused] = useState(false);
+  const inputBoxWidth = width < 360 ? width * 0.8 : 320;
 
-
-    return (
-        <ThemedView
-            style={[
-                styles.container,
-                { width: inputBoxWidth },
-                isFocused && styles.focusedContainer,
-            ]}
-        >
-            <Image source={imgPath} />
-            <TextInput
-                style={styles.inputContainer}
-                placeholder={placeHolder}
-                placeholderTextColor="rgba(255, 255, 255, 0.5)"
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                underlineColorAndroid="transparent"
-            />
-        </ThemedView>
-    )
+  return (
+    <ThemedView
+      style={[
+        styles.container,
+        { width: inputBoxWidth },
+        isFocused && styles.focusedContainer,
+      ]}
+    >
+      <Image source={imgPath} />
+      <TextInput
+        style={styles.inputContainer}
+        placeholder={placeHolder}
+        placeholderTextColor="rgba(255, 255, 255, 0.5)"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        underlineColorAndroid="transparent"
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        height: 56,
-        backgroundColor: '#343435',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        borderRadius: 12,
-        gap: 20,
-        borderWidth: 1,
-        borderColor: 'transparent',
-    },
-    focusedContainer: {
-        borderColor: '#B3B1FF',
-    },
-    inputContainer: {
-        flex: 1,
-        height: 24,
-        color: 'white',
-    },
-})
+  container: {
+    flexDirection: "row",
+    height: 56,
+    backgroundColor: "#343435",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    gap: 20,
+    borderWidth: 1,
+    borderColor: "transparent",
+  },
+  focusedContainer: {
+    borderColor: "#B3B1FF",
+  },
+  inputContainer: {
+    flex: 1,
+    height: 24,
+    color: "white",
+  },
+});
